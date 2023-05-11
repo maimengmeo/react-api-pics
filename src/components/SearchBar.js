48e2054a312f3a16f8f9d8a64ce48cfdbbb25a7e
maimengmeo <mai291294@gmail.com>
2023-05-10 22:47:23 -0400

    handled form submission

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,16 @@
-function SearchBar() {
-    return <div>Search Bar</div>;
+function SearchBar({ onSubmit }) {
+    //onSubmit from App
+    const handleFormSubmit = (event) => {
+        event.preventDefault(); //when enter key press, req wont be send
+        onSubmit("cars");
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleFormSubmit}>
+                <input />
+            </form>
+        </div>
+    );
 }
-export default App;
+export default SearchBar;
